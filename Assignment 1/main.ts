d431227cb96595d0d03d10b3afba903e937eac9c
Manahil AFAQ <manahilafaq281@gmail.com>
2024-03-16 07:29:44 +0500

    first commit

---
+++ Assignment 1/main.ts
@@ -0,0 +1,12 @@
+// ASSIGNMENT # 1
+
+//Storing a person name in a variable
+
+let personName = "Manahil";
+console.log(personName)
+
+//Printing a message to the person
+
+let message:string = "hello would you like to learn some python today?";
+console.log(personName,message)
+
---
+++ assignment 2/main.js
@@ -0,0 +1,9 @@
+//Assignment 2
+//Storing a person name in a variable
+var personName = "Manahil";
+//Printing a person name in lowercase
+console.log("Lowercase:", personName.toLowerCase());
+//printing a person name in uppercase
+console.log("Uppercase:", personName.toUpperCase());
+//printing a person name in titile case
+console.log("Titlecase:", personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
